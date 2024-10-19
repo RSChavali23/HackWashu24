0bbdf2cfa843e191c0caa9fc42cbfff4af613525
Austin Perez <a.m.perez@wustl.edu>
2024-10-18 20:38:12 -0500

    initial push

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{message}</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
